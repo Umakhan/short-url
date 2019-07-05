256f89fff38809d098cf5b34730a634b4b1504d5
Umakhan Saipudinov <abrakad55@gmail.com>
2019-07-05 19:22:16 +0300

    Added model Url

---
+++ db/migrate/20190705083539_create_urls.rb
@@ -0,0 +1,12 @@
+class CreateUrls < ActiveRecord::Migration[5.2]
+  def change
+    create_table :urls do |t|
+      t.string :original
+      t.string :short
+
+      t.timestamps
+    end
+
+    add_index :urls, :short
+  end
+end
